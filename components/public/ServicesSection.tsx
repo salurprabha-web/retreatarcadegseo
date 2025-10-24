cc0b3437a993ab21c3d27cd4fe7b2246569ebe3b
salurprabha-web <salurprabha@gmail.com>
2025-10-24 18:52:49 +0530

    Update ServicesSection.tsx

--- components/public/ServicesSection.tsx
+++ components/public/ServicesSection.tsx
@@ -20,8 +20,8 @@ const ServicesSection: React.FC<ServicesSectionProps> = ({ services }) => {
               href={`#/services/${service.seo.slug}`}
               className="block bg-brand-dark rounded-lg overflow-hidden shadow-lg transform hover:-translate-y-2 transition-transform duration-300 cursor-pointer group"
             >
-              <div className="overflow-hidden">
-                <img src={service.image_url} alt={service.name} className="w-full h-64 object-cover group-hover:scale-105 transition-transform duration-300" />
+              <div className="overflow-hidden aspect-video bg-brand-dark">
+                <img src={service.image_url} alt={service.name} className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300" />
               </div>
               <div className="p-8">
                 <h3 className="text-2xl font-bold text-brand-accent font-poppins mb-2">{service.name}</h3>
