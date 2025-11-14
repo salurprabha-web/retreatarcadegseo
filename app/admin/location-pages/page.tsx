1d798e282ccd0df5a324e681d3c9418ad34ceaeb
salurprabha-web <salurprabha@gmail.com>
2025-11-14 07:03:26 +0530

    Update page.tsx

--- app/admin/location-pages/page.tsx
+++ app/admin/location-pages/page.tsx
@@ -7,6 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import Link from "next/link";
 import { toast } from "sonner";
 
 type Location = {
