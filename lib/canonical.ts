c728306b344acab287ddf3d7a1e4a7cd3d525674
salurprabha-web <salurprabha@gmail.com>
2025-11-16 00:11:33 +0530

    Update canonical.ts

--- lib/canonical.ts
+++ lib/canonical.ts
@@ -1,7 +1,15 @@
 // lib/canonical.ts
-export function buildCanonical(productType, productSlug, locationSlug?) {
-  if (!locationSlug)
+
+export function buildCanonical(
+  productType: "event" | "service",
+  productSlug: string,
+  locationSlug?: string | null
+): string {
+  if (!productSlug) return "";
+
+  if (!locationSlug) {
     return `https://www.retreatarcade.in/${productType}s/${productSlug}`;
+  }
 
   return `https://www.retreatarcade.in/${productType}s/${productSlug}/${locationSlug}`;
 }
