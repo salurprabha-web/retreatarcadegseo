c0186203b0bea348c9b70b721e0d58033ea7ae3b
salurprabha-web <salurprabha@gmail.com>
2025-11-18 15:54:23 +0530

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,18 +1,14 @@
 import { cookies } from "next/headers";
-import { createServerClient as createSupabaseServerClient } from "@supabase/auth-helpers-nextjs";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 
 export function createServerClient() {
   const cookieStore = cookies();
 
-  return createSupabaseServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-      },
-    }
-  );
+  return createPagesServerClient({
+    cookies: {
+      get: (key: string) => cookieStore.get(key)?.value,
+      set: () => {},
+      remove: () => {},
+    },
+  });
 }
